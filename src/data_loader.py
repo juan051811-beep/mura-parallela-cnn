fd839adf920e7a6ac93006138fcf3fb1938a985d
juan051811-beep <juan051811@gmail.com>
2026-05-05 09:46:45 -0600

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,22 +1,22 @@
 import os
 
-def cargar_rutas(dataset_path):
-    rutas = []
-    etiquetas = []
+def cargar_rutas(dataset_path): #funcionque obtiene rutas de imágenes y sus etiquetas
+    rutas = [] #para guardar las rutas de las imágenes
+    etiquetas = []  #para guardar las etiquetas (0 o 1)
 
-    for root, _, files in os.walk(dataset_path):
-        for file in files:
-            if file.lower().endswith((".png", ".jpg", ".jpeg")):
-                ruta = os.path.join(root, file)
-                rutas.append(ruta)
+    for root, _, files in os.walk(dataset_path): #recorremos todas las carpetas del dataset
+        for file in files: #recorremos todos los archivos dentro de cada carpeta
+            if file.lower().endswith((".png", ".jpg", ".jpeg")): #filtramos solo archivos de imagen
+                ruta = os.path.join(root, file) #construimos la ruta completa de la imagen
+                rutas.append(ruta) #guardamosla ruta en la lista
 
-                ruta_lower = ruta.lower()
+                ruta_lower = ruta.lower() #convertimos la ruta a minúsculas para comparar
 
-                if "positive" in ruta_lower:
-                    etiquetas.append(1)
-                elif "negative" in ruta_lower:
-                    etiquetas.append(0)
+                if "positive" in ruta_lower:  #si la ruta contiene "positive"
+                    etiquetas.append(1)  #se asigna etiqueta 1 (anormal)
+                elif "negative" in ruta_lower:  #si contiene "negative"
+                    etiquetas.append(0)  #se asigna etiqueta 0 (normal)
                 else:
-                    etiquetas.append(-1)
+                    etiquetas.append(-1)  #si no se identifica, se marca como -1
 
-    return rutas, etiquetas
+    return rutas, etiquetas  #devolvemos listas de rutas y etiquetas
