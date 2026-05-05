6f3af1bb28950956c71f858f97ef83c480585ee8
juan051811-beep <juan051811@gmail.com>
2026-05-05 09:51:06 -0600

    Update test_loader.py

--- src/test_loader.py
+++ src/test_loader.py
@@ -1,40 +1,39 @@
-import pandas as pd
-from data_loader import cargar_rutas
+import pandas as pd  
+from data_loader import cargar_rutas 
 from chunks import dividir_chunks
 
-rutas, etiquetas = cargar_rutas("data/MURA-v1.1")
+rutas, etiquetas = cargar_rutas("data/MURA-v1.1") #cargamos la rita y las etiquetas del dataset
 
-print("Total imágenes:", len(rutas))
-print("Normal:", etiquetas.count(0))
-print("Abnormal:", etiquetas.count(1))
-print("Sin etiqueta:", etiquetas.count(-1))
-
-
-chunks = dividir_chunks(rutas, etiquetas, 4)
-
-print("\nChunks:")
-for i, chunk in enumerate(chunks):
-    print(f"Chunk {i+1}: {len(chunk['rutas'])} imágenes")
+print("Total imágenes:", len(rutas))#mostramos el número total de imágenes
+print("Normal:", etiquetas.count(0)) #contamos cuántas son normales
+print("Abnormal:", etiquetas.count(1)) #contamos cuántas son anormales
+print("Sin etiqueta:", etiquetas.count(-1)) #contamos si hay etiquetas no identificadas
 
 
-total_chunks = sum(len(c['rutas']) for c in chunks)
+chunks = dividir_chunks(rutas, etiquetas, 4) #dividimos el dataset en 4 partes (solo prueba)
+
+print("\nChunks:") #mostramos los chunks
+for i, chunk in enumerate(chunks): #recorremos cada chunk
+    print(f"Chunk {i+1}: {len(chunk['rutas'])} imágenes") #tamaño de cada chunk
+
+
+total_chunks = sum(len(c['rutas']) for c in chunks) #suma total de elementos en todos los chunks
 
 print("\nValidación:")
-print("Total original:", len(rutas))
-print("Total en chunks:", total_chunks)
+print("Total original:", len(rutas)) #total original de imágenes
+print("Total en chunks:", total_chunks) #total después de dividir
 
-if total_chunks == len(rutas):
-    print("Bien, no se perdieron datos")
+if total_chunks == len(rutas): #verificamos que no se hayan perdido datos
+    print("Bien, no se perdieron datos") 
 else:
-    print("Error")
+    print("Error") 
 
 
-
-df = pd.DataFrame({
+df = pd.DataFrame({ #creamos un DataFrame con rutas y etiquetas
     "ruta": rutas,
     "etiqueta": etiquetas
 })
 
-df.to_csv("data/metadata.csv", index=False)
+df.to_csv("data/metadata.csv", index=False) #guardamos el metadata en archivo CSV
 
-print("\n metadata.csv guardado")
+print("\n metadata.csv guardado") 
